449ef622601480c7c230b4860f451e9e5dffa7e2
CharleyBlue <charleyblue@charleybluedesigns.com>
2022-09-15 21:41:32 -0700

    Added webpack, webpack.config.js, added output file named bundled.js to index.html

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+// let fileSys = require('fs');
+// fileSys.writeFile(__dirname + '/about.html', '<h1>About Us</h1>', function(error) {
+//     if(error) {
+//         return console.log(error);
+//     } else {return console.log('Congrats!')};
+// });
+alert('Hello...Testing 4321!');
---
+++ app/bundled.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app/assets/scripts/App.js":
+/*!***********************************!*\
+  !*** ./app/assets/scripts/App.js ***!
+  \***********************************/
+/***/ (() => {
+
+eval("// let fileSys = require('fs');\r\n// fileSys.writeFile(__dirname + '/about.html', '<h1>About Us</h1>', function(error) {\r\n//     if(error) {\r\n//         return console.log(error);\r\n//     } else {return console.log('Congrats!')};\r\n// });\r\nalert('Hello...Testing 4321!');\n\n//# sourceURL=webpack://backatitcharleyblue/./app/assets/scripts/App.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./app/assets/scripts/App.js"]();
+/******/ 	
+/******/ })()
+;
